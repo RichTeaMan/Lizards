90dcdeb53f1a4bf7ea5d978635911e5f1601a64f
Thomas Holmes <thomasholmes@outlook.com>
2020-01-03 13:48:19 +0000

    Added turn message.

--- src/endTurn/EndTurnConsumer.ts
+++ src/endTurn/EndTurnConsumer.ts
@@ -6,6 +6,8 @@ import { ToastPayload } from "../toast/ToastPayload";
 export class EndTurnConsumer implements Consumer<EndTurnMessagePayload> {
 
     static readonly messageType = "endTurn";
+    private turnMessage: Phaser.GameObjects.Text;
+    private readonly turnMessageY = 100;
 
     fetchMessageType(): string {
         return EndTurnConsumer.messageType;
@@ -13,11 +15,13 @@ export class EndTurnConsumer implements Consumer<EndTurnMessagePayload> {
 
     consume(simulationScene: SimulationState, payload: EndTurnMessagePayload): void {
 
+
         // get next team
         const nextTeam = simulationScene.fetchNextTeam();
         if (!nextTeam) {
             // TODO: current team has won
             simulationScene.messageRegister.registerMessage(ToastPayload.createToast(`${simulationScene.selectedLizard.team.name} wins!`));
+            this.updateTurnMessage(simulationScene, `${simulationScene.selectedLizard.team.name} wins!`, simulationScene.selectedLizard.team.colour);
         }
         else {
             const nextCombatant = nextTeam.fetchNextCombatant();
@@ -27,8 +31,23 @@ export class EndTurnConsumer implements Consumer<EndTurnMessagePayload> {
             else {
                 simulationScene.selectedLizard = nextCombatant;
                 simulationScene.messageRegister.registerMessage(ToastPayload.createToast(`${nextCombatant.team.name}, ${nextCombatant.name}'s turn!`));
+
+                this.updateTurnMessage(simulationScene, `${nextCombatant.team.name}, ${nextCombatant.name}'s turn!`, nextCombatant.team.colour);
             }
         }
+    }
+
+    private updateTurnMessage(simulationScene: SimulationState, message: string, colour: string) {
+        if (!this.turnMessage) {
+            this.turnMessage = simulationScene.uiScene.add.text(0, this.turnMessageY, "");
+        }
+
+        this.turnMessage.text = message;
+        this.turnMessage.setColor(colour);
+
+        // center horizontally in screen
+        const x = (simulationScene.uiScene.cameras.main.width / 2) -(this.turnMessage.displayWidth / 2);
+        this.turnMessage.x = x;
     }
 
 }
